b57505038f5bf7345f64af126186dfa34d51a3e5
demik.github.io <devanshmushra29@gmail.com>
2026-07-31 01:07:21 +0530

    Time: 1 ms (41.21%), Space: 63.6 MB (8.1%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,41 @@
+// class Solution {
+//     // public void rotate(int[] nums, int k) {
+//     //     int n=nums.length;
+//     //     k%=n;
+       
+//     //     reverse(nums, 0, n - 1);
+//     //     reverse(nums, 0, k - 1);
+//     //     reverse(nums, k, n - 1);
+//     //     System.out.println(Arrays.toString(nums));
+
+      
+//     // }
+//     // private void reverse(int[]nums,int left,int right){
+//     //     while(left<= right){
+//     //         int temp=nums[left];
+//     //         nums[left]= nums[right];
+//     //         nums[right]= temp;
+//     //         left++;
+//     //         right--;
+//     //     }
+//     // }
+
+// }
+
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
