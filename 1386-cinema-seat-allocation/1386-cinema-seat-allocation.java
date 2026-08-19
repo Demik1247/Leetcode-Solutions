179a09dbb2c5db1039a95f9990335649d81c910d
demik.github.io <devanshmushra29@gmail.com>
2026-08-19 11:02:15 +0530

    Time: 19 ms (71.05%), Space: 50.5 MB (96.84%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
+
+            map.put(row, map.getOrDefault(row, 0) | (1 << col));
+        }
+
+        int ans = 2 * n;
+
+        for (int mask : map.values()) {
+            boolean left = (mask & ((1 << 2) | (1 << 3) | (1 << 4) | (1 << 5))) == 0;
+            boolean middle = (mask & ((1 << 4) | (1 << 5) | (1 << 6) | (1 << 7))) == 0;
+            boolean right = (mask & ((1 << 6) | (1 << 7) | (1 << 8) | (1 << 9))) == 0;
+
+            if (left && right) {
+                // Can place two groups
+                continue;
+            } else if (left || middle || right) {
+                // Can place one group instead of the default two
+                ans--;
+            } else {
+                // Cannot place any group
+                ans -= 2;
+            }
+        }
+
+        return ans;
+    }
+}
